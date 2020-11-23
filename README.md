9b19e1deb277bcab94c0f6e6ddea4bdebfdff063
Bright Chung <bywoch@gmail.com>
2020-11-23 17:00:00 +0900

    update PRL

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# press_release_list
+# Press Release List
 외부 API에서 데이터 목록을 검색하고 JavaScript의 AJAX를 사용하여 처리
--- js/common.js
+++ js/common.js
@@ -1,3 +1,28 @@
 $(document).ready(function () {
+    window.onload = function () {
+        $('.pr-slide').slick({
+            infinite: true,
+            slidesToShow: 4,
+            slidesToScroll: 1,
+            dots: false,
+            arrows: true,
+            draggable: false
+        });
+        $(".main-pr .pr-slide .slick-prev").text('이전');
+        $(".main-pr .pr-slide .slick-next").text('다음');
 
+        // slick pr 웹 접근성
+        $('.slick-list .slick-active, .slick-list .slick-slide a').removeAttr('tabindex');
+        $('.slick-list .item a').hide();
+        $('.slick-list .slick-active a').show();
+        $('.slick-list .slick-active').last().next().find('a').show();
+        $('.slick-list .slick-current').prev().find('a').show();
+
+        $('.slick-arrow').click(function () {
+            $('.slick-list .slick-active a').show();
+            $('.slick-list .slick-active').last().next().find('a').show();
+            $('.slick-list .slick-current').prev().find('a').show();
+        });
+
+    }
 });
