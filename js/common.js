6cec582424706f16f3383d7cd451ce5a2f07eb02
Bright Chung <bywoch@gmail.com>
2020-11-24 14:00:00 +0900

    update PRL

--- js/common.js
+++ js/common.js
@@ -1,28 +1,53 @@
 $(document).ready(function () {
-    window.onload = function () {
-        $('.pr-slide').slick({
-            infinite: true,
-            slidesToShow: 4,
-            slidesToScroll: 1,
-            dots: false,
-            arrows: true,
-            draggable: false
-        });
-        $(".main-pr .pr-slide .slick-prev").text('이전');
-        $(".main-pr .pr-slide .slick-next").text('다음');
 
-        // slick pr 웹 접근성
-        $('.slick-list .slick-active, .slick-list .slick-slide a').removeAttr('tabindex');
-        $('.slick-list .item a').hide();
-        $('.slick-list .slick-active a').show();
-        $('.slick-list .slick-active').last().next().find('a').show();
-        $('.slick-list .slick-current').prev().find('a').show();
+    $.ajax({
+        url: "https://LoremIpsum.com/api/press/list",
+        dataType: "jsonp",
+        async: true,
+        cache: true,
+        data: {
+            opt: 0,
+            keyword: "",
+            page: 1,
+            size: 16
+        },
+        success: function (d) {
+            var azHtml = [];
+            var o = d.list;
+            for (var i = 0; i < o.length; i++) {
+                azHtml.push('<div class="item">');
+                if (o[i].photo_fullpath.length > 0) {
+                    azHtml.push('<a href="/pr/press/view.html?idx=' + o[i].code + '"><span class="img"><img src="https://LoremIpsum.com/data/' + o[i].photo_fullpath + '" alt=""></span><p class="txt"><span class="ellipsis-multi">' + o[i].title + '</span></p></a>');
+                }
+                else {
+                    azHtml.push('<a href="/pr/press/view.html?idx=' + o[i].code + '"><span class="img"><img src="https://LoremIpsum.com/images/pr_default.jpg" alt=""></span><p class="txt"><span class="ellipsis-multi">' + o[i].title + '</span></p></a>');
+                }
+                azHtml.push('</div>');
+            }
+            $(".pr-slide").html(azHtml.join(''));
+            $('.pr-slide').slick({
+                infinite: true,
+                slidesToShow: 4,
+                slidesToScroll: 1,
+                dots: false,
+                arrows: true,
+                draggable: false
+            });
+        },
+        error: function (request, status, error) {
+        }
+    });
 
-        $('.slick-arrow').click(function () {
-            $('.slick-list .slick-active a').show();
-            $('.slick-list .slick-active').last().next().find('a').show();
-            $('.slick-list .slick-current').prev().find('a').show();
-        });
+    /* $('.pr-slide').slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        dots: false,
+        arrows: true,
+        draggable: false
+    }); */
 
-    }
+    $(".main-pr .pr-slide .slick-prev").text('이전');
+    $(".main-pr .pr-slide .slick-next").text('다음');
+
 });
